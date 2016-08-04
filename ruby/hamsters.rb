f2814155d62c67f0d93f6ec92b92da16fb3afc9e
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-03 19:30:48 -0700

    Intial commit - release 1 and release 2 4.3

---
+++ ruby/hamsters.rb
@@ -0,0 +1,14 @@
+puts "What is the hamster's name?"
+hamsters_name = gets.chomp
+
+puts "What is the hamster's volume level? (From 1 to 10)"
+hamsters_volume = gets.chomp.to_i
+
+puts "What is the fur color of the hamster?"
+hamster_color = gets.chomp
+
+puts "Is the hamster a good candidate for adoption? (yes/no)"
+hamster_good = gets.chomp
+
+puts "What is the estimated age of the hamster?"
+hamster_age = gets.chomp
