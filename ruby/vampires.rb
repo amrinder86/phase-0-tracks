071f71f446a2116aa2d032aa7e50d7f174632231
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-04 14:22:40 -0700

    Initial Commit -added code to ask for input to gather data

---
+++ ruby/vampires.rb
@@ -0,0 +1,17 @@
+puts " What is your name?"
+  name = gets.chomp
+puts "How old are you? What year were you born?"
+  age_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  garlic_bread = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+  health_insurance = gets.chomp
+
+  
+
+  puts "Employee Name = #{name}
+      Age & Year born = #{age_year}
+      Garlic Bread Order = #{garlic_bread}
+      Health Insurance = #{health_insurance}"
+
+      
