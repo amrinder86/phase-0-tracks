53bd44fb70bd0bd128351e9530258e162919b431
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-05 15:46:06 -0700

    Add If statements and year

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,17 +1,74 @@
-puts " What is your name?"
+ 
+  
+ current_year =2016
+
+  puts " What is your name?"
   name = gets.chomp
+ 
 puts "How old are you? What year were you born?"
   age_year = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+ 
+  if age_year > "100" 
+    age_year = true
+  else 
+    age_year = false
+  end
+  puts " #{name}, What year were you born in?"
+ year_born = gets.chomp
+    
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
   garlic_bread = gets.chomp
+  
+  if garlic_bread == "no"
+    garlic_bread = false
+  else
+    garlic_bread = true
+  end
+
 puts "Would you like to enroll in the company’s health insurance?"
   health_insurance = gets.chomp
-
+  if health_insurance == "no"
+    health_insurance = false
+  else
+    health_insurance = "true"
+  end
+  
+   if age_year && (garlic_bread || health_insurance)
+  
+      puts "Probably not a vampire."
+     
+     elsif age_year && (garlic_bread || health_insurance)
+  
+  
+  puts "Probably a vampire."
+  
+  end
+  
+  if name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a Vampire"
+  else 
+    print “Results inconclusive.”
+  end
+  
+  
+ end
   
 
-  puts "Employee Name = #{name}
-      Age & Year born = #{age_year}
-      Garlic Bread Order = #{garlic_bread}
-      Health Insurance = #{health_insurance}"
+  
+ 
+  
+  
+   
+    
 
-      
+  
+  
+  
+  
+
+  
+ 
+  
+  
+   
+    
