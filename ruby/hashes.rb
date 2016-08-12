1d227d0bf995b4fd91846c9392b8c1e8b0cdd830
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-11 20:55:51 -0700

    Finish 5.3 - Cleaned code and added more puts to make it more presentable

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -19,9 +19,6 @@
 #print the hash
 
 #creating a hash and gathering data
-
-
-  
 client_info = {}
 
 puts "what is your name?"
@@ -40,44 +37,51 @@ puts "Would you like a small tv in the kitchen? (y/n)"
     client_info[:kitchen_tv] = gets.chomp
 if client_info[:kitchen_tv] == "n"
     client_info[:kitchen_tv] = false
-else 
+else client_info[:kitchen_tv] == "y"
     client_info[:kitchen_tv] = true
-  end
-puts "Here is what you have told us so far:"
-p client_info
+end
 
-puts "would you like to make any changes to your provided info?(yes/no)"
+puts "Here is what you have told us so far:"
+  p client_info
+
+puts "would you like to make any changes to your provided info?If no write none other write yes (yes or none)"
   response = gets.chomp
-if response == "no"
+if response == "none"
+  # printing the Client info if client says no changes.
   puts "Here is your information again."
     p client_info
-  puts "Thank you very much for your info.We will give you a quote in 24 hours."
-  
- elsif response == "yes"
-  puts "what would you like to update: Select from  name age children decor_theme  kitchen_tv"
+  puts "Thank you very much for your time.We will give you a quote in 24 hours."
+  #ask the client what they want to update and gives them the option to choose just one category
+  #updating values
+elsif response == "yes"
+  puts "what would you like to update: Select from  \nname \nage \nchildren \ndecor_theme  \nkitchen_tv"
   response2= gets.chomp
-  if response2 == "name"
+
+if response2 == "name"
+    puts "New name:"
   client_info[:name] = gets.chomp.to_s
-  elsif response2 == "age"
+
+elsif response2 == "age"
+    puts "Update age to:"
     client_info[:age] = gets.chomp.to_i
 
 elsif response2 == "children"
+  puts "Number of children:"
     client_info[:children] = gets.chomp.to_i
 
 elsif response2 == "decor_theme"
+  puts "Change decor theme to:"
     client_info[:decor_theme] = gets.chomp
 
 elsif response2 == "kitchen_tv"
+  puts "Update Kitchen tv preference to:(y/n)"
     client_info[:kitchen_tv] = gets.chomp
     
   
-  end
+end
+# printing the hash Client info
   puts "Here is your updated information."
   p client_info
   puts "Thank you very much for your info.We will give you a quote in 24 hours."
-
-
-
-
 end
 
