be11e88b3e8f0fbdd394635263d4a8b7efd8b240
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-11 18:12:25 -0700

    Add pseudocode to hashes.rb

---
+++ ruby/hashes.rb
@@ -0,0 +1,19 @@
+#Ask the clients for details
+#Client's name (string/symbol)
+#age (integer)
+#number of children (integer)
+#Decor theme (string/symbol)
+#Colors they hate or love 
+#We can have our own as long as it's good 
+#mix of string, integer and boolean data
+
+#store and display all information
+#convert input to appropriate data(symbol/string)
+#print the hash with all the information
+#update a key
+#create a If statement here. if designer says 
+#none skip it other have the value of key pop up
+#and have he designer fill it and update he value of it 
+
+#print the hash
+
