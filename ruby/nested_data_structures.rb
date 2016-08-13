a2b8d80b57f2de7f4c6055e94f529c7f1cd8e842
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-12 19:15:14 -0700

    Finish ruby 5.5 - cleaned code

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,3 +1,8 @@
+# Customer is wondering which level is best to watch the game and what suggestions are provided by stadium support.
+# Create a nested structure with array and hashes 
+# Write few calls to access various parts the nested structure.
+
+
 cricket_stadium = {
   level_1: {
     rows: ["first 40 rows", "yellow color"],
@@ -47,19 +52,21 @@ Media_box_level: {
 puts "How long is level 1?"
 puts  "It is #{cricket_stadium[:level_1][:rows][0]}"
 puts""
+
 puts "what color are all the levels in stadium?"
 puts "Level 1 is #{cricket_stadium[:level_1][:rows][1]}.
 Level 2 is #{cricket_stadium[:level_2][:rows][1]}.
 level 3 is #{cricket_stadium[:level_3][:rows][1]}.
 Media Box Level is #{cricket_stadium[:Media_box_level][:rows][1]}."
 puts""
+
 puts "Which level has the best view to watch the game of Cricket?"
 puts "Level 1 has #{cricket_stadium[:level_1][:Visibility_level][0]}. \nSuggestion/Advice : #{cricket_stadium[:level_1][:Visibility_level][1]}.
 At Level 2 you get a #{cricket_stadium[:level_2][:Visibility_level][0]}. \nSuggestion/Advice : #{cricket_stadium[:level_2][:Visibility_level][1]}.
 At level 3 It's #{cricket_stadium[:level_3][:Visibility_level][0]}. \nSuggestion/Advice : #{cricket_stadium[:level_3][:Visibility_level][1]}.
 At Media Box Level the view is #{cricket_stadium[:Media_box_level][:Visibility_level][0]}. \nSuggestion/Advice : #{cricket_stadium[:Media_box_level][:Visibility_level][1]}."
 puts""
-puts""
+
 puts "How many seats are available at each level as of now?"
 puts "Number of seats available as of now are:
 Level 1 = #{cricket_stadium[:level_1][:seating_info].values_at(:seats_available_for_Sale)} Seats.
