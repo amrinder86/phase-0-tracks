87967ac233772c920aba25a507ec661b742649e5
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-14 14:23:43 -0700

    Initial commit - Psuedocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,7 @@
+# write a method that takes a names and turn it into a fake name
+# swap first and last name 
+# change vowels "aeiou" to next vowel "eioua"
+# change all the consonants to next consonant except vowels
+
+vowels = [aeiou]
+consonants = [bcdfghjklmnpqrstvwxyz]
