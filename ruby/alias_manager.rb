a893e3454b1fc8bfe1374c136b54e5b32941344b
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-14 21:46:13 -0700

    Add hash to store data but code is broken

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,53 +4,49 @@
 # change all the consonants to next consonant except vowels
 
 
-
+storing_names = {}
 loop do
-puts "please answer the following questions. Press 'ENTER' to continue or Type 'quit' to exit."
-input = gets.chomp
+  puts "please answer the following questions. Press 'ENTER' to continue or Type 'quit' to exit."
+    input = gets.chomp
 break if input == 'quit'
 
 puts "What's your first name?"
-first_name = gets.chomp.downcase
+  first_name = gets.chomp.downcase
 puts "what's your last name?"
-last_name = gets.chomp.downcase
+  last_name = gets.chomp.downcase
 
 real_name=last_name+" "+first_name
 
 def name_method(real_name)
-vowels = "aeiou"
-consonants = "bcdfghjklmnpqrstvwxyz"
+    vowels = "aeiou"
+    consonants = "bcdfghjklmnpqrstvwxyz"
 
+  vowels.split("")
+    split_real_name = real_name.split("")
 
+  modified_name = []
 
-vowels.split("")
-split_real_name = real_name.split("")
+  split_real_name.map! do |word|
 
-modified_name = []
-
-split_real_name.map! do |word|
-
-if vowels.include?(word)
-  checking_1 = vowels[vowels.index(word)+1]
-  modified_name << checking_1
-elsif consonants.include?(word)
-  checking_2 = consonants[consonants.index(word)+1]
-  modified_name << checking_2
-elsif word == " "
-  checking_3 = " "
-  modified_name << checking_3
+      if vowels.include?(word)
+          checking_1 = vowels[vowels.index(word)+1]
+            modified_name << checking_1
+      elsif consonants.include?(word)
+          checking_2 = consonants[consonants.index(word)+1]
+            modified_name << checking_2
+      elsif word == " "
+          checking_3 = " "
+            modified_name << checking_3
 end 
   
-  end
-  modified_name.join("").split.map{|word| word.capitalize}.join(" ")
+end
+      modified_name.join("").split.map{|word| word.capitalize}.join(" ")
 end
 
-
-
-
-
-puts "Your spy name is - " +name_method(real_name)
+      puts "Your spy name is - " +name_method(real_name)
+      storing_names[real_name] = modified_name
 end
 
-
-
+    storing_names.each do |real_name, modified_name|
+  puts " #{real_name} is #{modified_name}!"
+end
