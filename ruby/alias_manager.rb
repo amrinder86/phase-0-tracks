7d03dc7665954adc49802d9d4052a84ecc585aa5
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-14 21:23:29 -0700

    Added method for next vowel and consonants

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,41 +3,54 @@
 # change vowels "aeiou" to next vowel "eioua"
 # change all the consonants to next consonant except vowels
 
-vowels = "aeiou"
-consonants = "bcdfghjklmnpqrstvwxyz"
-
-def name_method(letter)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 loop do
-puts"please answer the following questions. Type anything to continue or Type 'quit' to exit."
+puts "please answer the following questions. Press 'ENTER' to continue or Type 'quit' to exit."
 input = gets.chomp
 break if input == 'quit'
-puts "What's your first name ?"
+
+puts "What's your first name?"
 first_name = gets.chomp.downcase
 puts "what's your last name?"
 last_name = gets.chomp.downcase
 
-full_name=last_name+" "+first_name
-p full_name 
+real_name=last_name+" "+first_name
 
+def name_method(real_name)
+vowels = "aeiou"
+consonants = "bcdfghjklmnpqrstvwxyz"
+
+
+
+vowels.split("")
+split_real_name = real_name.split("")
+
+modified_name = []
+
+split_real_name.map! do |word|
+
+if vowels.include?(word)
+  checking_1 = vowels[vowels.index(word)+1]
+  modified_name << checking_1
+elsif consonants.include?(word)
+  checking_2 = consonants[consonants.index(word)+1]
+  modified_name << checking_2
+elsif word == " "
+  checking_3 = " "
+  modified_name << checking_3
+end 
+  
+  end
+  modified_name.join("").split.map{|word| word.capitalize}.join(" ")
 end
+
+
+
+
+
+puts "Your spy name is - " +name_method(real_name)
+end
+
+
 
