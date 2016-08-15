19c2a68db5cb4b10885584631941356080059416
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-15 16:47:38 -0700

    Finished 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#Puppy ------------------------
+#pug
+
+#CHARACTERISTICS ----------------
+
+#Friendly: yes
+#Eye_count: 2
+#Name: varies
+#Eye_Color: black
+#adorable: yes
+#adoptable:yes
+
+#BEHAVIOR -----------------------
+#quiet
+#slow
+#playful
