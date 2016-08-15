e8b64b5efaedf6d1acfe1a319c7e1c0aa45debb8
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-14 18:33:58 -0700

    Add loop and input statements

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,5 +3,41 @@
 # change vowels "aeiou" to next vowel "eioua"
 # change all the consonants to next consonant except vowels
 
-vowels = [aeiou]
-consonants = [bcdfghjklmnpqrstvwxyz]
+vowels = "aeiou"
+consonants = "bcdfghjklmnpqrstvwxyz"
+
+def name_method(letter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+loop do
+puts"please answer the following questions. Type anything to continue or Type 'quit' to exit."
+input = gets.chomp
+break if input == 'quit'
+puts "What's your first name ?"
+first_name = gets.chomp.downcase
+puts "what's your last name?"
+last_name = gets.chomp.downcase
+
+full_name=last_name+" "+first_name
+p full_name 
+
+end
+
