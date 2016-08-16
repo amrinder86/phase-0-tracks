415536db1afd788c48eabdfe0e72cbdc4053d009
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-15 19:10:12 -0700

    Initial Commit - Add class and 3 methods

---
+++ ruby/santa.rb
@@ -0,0 +1,14 @@
+class Santa
+    def speak(word)
+     puts "Ho, ho, ho! Haaaappy holidays!"
+  end 
+   
+    def eat_milk_and_cookies(cookie)
+      puts "That was a good #{cookie}"
+     
+   end
+   def initialize
+    p "Initializing Santa instance ..."
+     
+   end
+end
