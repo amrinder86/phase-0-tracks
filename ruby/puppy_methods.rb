68526176fa842091ea1b729f612634786831ab01
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-16 17:36:12 -0700

    Initialize Speak and Roll_over method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,20 @@
 class Puppy
-
+ 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
+  def speak(integer)
+    
+    integer.to_i.times{puts "Woof!"} 
+  end
+  def roll_over
+    puts "*rolls over*"
+    
+  end
 
 end
-
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.speak(5)
+puppy.roll_over
