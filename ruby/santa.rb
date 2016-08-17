018b2aca3227675b4848f3e5cecd18dc7da7ece7
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-17 12:24:39 -0700

    Add getter and setter methods and test the code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,6 +4,7 @@ class Santa
     @ethnicity = ethn
     
     @age = 0
+    
     p "Initializing Santa instance ..."
   end
     def speak(word)
@@ -11,16 +12,49 @@ class Santa
   end 
    
     def eat_milk_and_cookies(cookie)
-      puts "That was a good #{cookie}"
+      puts "That was a good #{cookie}."
      
+    end
+   # setter methods
+    def celebrate_birthday=(integer)
+      @age = integer+1
+    end
+
+    def get_mad_at=(name)
+      last_ranking = name.last
+      
+    end
+    def gender=(word)
+      @gender = word
+      
+    end
+    #getter methods
+    def age
+      @age
+      end
+      
+    def ethnicity
+      @ethnicity
+      
+    end
+   def genderi
+    @gender
    end
-   
 end
 reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 santas = []
-santa = Santa.new 
+santa = Santa.new("agender", "black")
 santa.speak("kaka")
 santa.eat_milk_and_cookies("snickerdoodle")
+puts "Santa is #{santa.age} years old and has #{santa.ethnicity} ethnicity"
+puts "Santa with gender '#{santa.genderi}'is ready."
+puts "============================================"
+santa.gender="kalput"
+puts "Santa with gender '#{santa.genderi}'is ready."
+puts "============================================"
+santa.celebrate_birthday=0
+puts "Santa is now #{santa.age} years old and has #{santa.ethnicity} ethnicity"
 
-santas << Santa.new("agender", "black")
+
+
 
