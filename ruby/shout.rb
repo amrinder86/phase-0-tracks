f86dac9fb28afda02b998b2aa3fbadb2af48d66d
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-19 16:53:41 -0700

    Add driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,8 @@
 module Shout
   def self.yell_angrily(words)
-    words + "!!!" + " :("
+    words + "!!!!!!!!!!" + "**someone starts crying**"
   end
 end
+
+# driver code 
+ Shout.yell_angrily("This is not good at all")
