f3fdd96c178ba874c29bc404dfe82693b7a64f2b
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-20 13:40:55 -0700

    Initial commit - Done writing pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,9 @@
+# Designing a a custom laptop
+# how many laptops would u like to build
+# ask the user what brand of PC do they want (Hp, Apple, Dell, lenevo etc..)
+# Ask user for what kind of processor brand they want (amd / intel)
+# how much ram would they like to have ( 4gb , 8gb ,16 gb etc)
+# how big the screen should be (15.5 inches or 13.3 inches etc)
+# any special Instructions like"keyboard should be backlit etc.)
+# create random price range from $700 to $2000.
+# provide the user with all laptop info with price.
