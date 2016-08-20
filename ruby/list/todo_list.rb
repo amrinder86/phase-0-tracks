92df474e6eaf196e72a57aa2fb12e9c3eff52f7e
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-20 16:59:59 -0700

    FInish Pair work

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,25 @@
+class TodoList
+
+  attr_accessor :list
+
+  def initialize(list)
+    @list = list
+  end
+
+  def get_items
+    p list
+  end
+
+  def add_item(item)
+    list << item
+  end
+
+  def delete_item(item)
+    list.delete(item)
+  end
+
+  def get_item(index)
+    p list[index]
+  end
+
+end
