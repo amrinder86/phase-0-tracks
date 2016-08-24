0cf85807bf4f7911cb88f563334db1c42cb0c3d2
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-24 13:03:30 -0700

    Add a backwards loop

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,18 @@
 // loop will run until the values if counter becomes zero 
 // we will use i-- to Decrement our counter by 1 so it goes from 
 // string's length to zero
+
+//var str = "Loop over me!";
+
+  //for (var i = str.length - 1; i >= 0; i--){
+    
+  //console.log(str[i]);}
+  
+  function reverse(string){
+
+  for (var i = string.length - 1; i >= 0; i--){
+    
+  console.log(string[i]);}
+}
+
+(reverse("hello")
