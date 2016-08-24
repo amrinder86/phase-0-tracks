f391318db4ada22582101ed9a0b522e9af04fa4b
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-08-24 12:50:20 -0700

    Intial commit - Pseudocode to loop backwards

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// task is to reverse a string
+// function will take a string and will return the string backwards
+// we will set the counter to string's length -1 
+// loop will run until the values if counter becomes zero 
+// we will use i-- to Decrement our counter by 1 so it goes from 
+// string's length to zero
